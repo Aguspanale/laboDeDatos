a4c57fe18b1ed7b0e5265471724ca4bbc2bbfee2
Aguspanale <124443351+Aguspanale@users.noreply.github.com>
2023-02-23 12:57:12 -0300

    Add files via upload

--- kmedias.R
+++ kmedias.R
@@ -1,67 +1,114 @@
 library(ggplot2)
-library(GGally)
-library(gridExtra)
 library(palmerpenguins)
 
-pengu <- penguins
+datos <- matrix(nrow = 8,ncol = 2)
 
-distancia <- function(a,b){
-  return(sqrt((a[1]-b[1])**2+(a[2]-b[2])**2))
+datos[1,] <- c(1,2)
+datos[2,] <- c(2,1)
+datos[3,] <- c(6,1)
+datos[4,] <- c(7,2)
+datos[5,] <- c(6,5)
+datos[6,] <- c(6,7)
+datos[7,] <- c(5,6)
+datos[8,] <- c(1,1)
+
+datosDF <- data.frame(datos)
+
+ggplot(datosDF,aes(X1,X2)) + geom_point()
+
+##para testear
+muestras <- datosDF
+k = 3
+
+###
+
+
+
+
+
+distanciaE <- function(a,b){
+  return((a[1]-b[1])**2+(a[2]-b[2])**2)
 }
 
-kmedias <- function(matriz,k.clus){
 
-  matriz <- matriz[!is.na(matriz[,1]) & !is.na(matriz[,2]),]
+
+kmedias <- function(muestras,k){
+  ## limpio los datos pasando a double y sacando los NA
   
-  centros <- matriz[sample(1:dim(matriz)[1],replace = FALSE,size = k.clus),]
+  muestras <- muestras[!is.na(muestras[,1]) & !is.na(muestras[,2]),]
+  
+  ## primero obtengo mis centros iniciales
+  
+  centros <- muestras[sample((1:dim(muestras)[1]),replace = FALSE, size = k),]
   centros[,1] <- as.double(unlist(centros[,1]))
   centros[,2] <- as.double(unlist(centros[,2]))
   
-  m <- cbind(matriz,NA)
-  m <- cbind(m,NA)
-  acum <- c()
-  k <- 1
-  print(centros)
-  while (TRUE){
+  ## armo una matriz para llenar con las distancias de todos los puntos a todos los centros
+  
+  distanciaACentros <- matrix(nrow = dim(muestras)[1],ncol = k)
+  n <- 1
+  error <- c()
+  while(n < 100){
     
-    #asigno cada punto a su centro
-    
-    for (i in (1:length(m[,1]))){
-      for (j in 1:k.clus){
-        if((is.na(m[i,3])) | (distancia(m[i,1:2], centros[j,]) < m[i,3])){
-          m[i,3] <- distancia(m[i,1:2], centros[j,])
-          m[i,4] <- j
-        }
+    for (i in 1:dim(muestras)[1]){
+      for (j in 1:k){
+        distanciaACentros[i,j] <- as.numeric(distanciaE(muestras[i,],centros[j,]))
+       
       }
     }
-    acum[k] <- sum(m[,3])
+
+   
     
+    ##hago un vector que para cada elemento tiene el indice de su centro actual (que será el que tenga mas "cerca")
     
-    #recalculo centros
+    centrosAsignados <- c()
     
-    for (l in 1:k.clus){
-      centros[l,1] <- mean(m[m[,4] == l,1])
-      centros[l,2] <- mean(m[m[,4] == l,2])
+    for (i in 1:dim(muestras)[1]){
+      centrosAsignados[i] <- which.min(distanciaACentros[i,])
     }
-    print(centros)
+    
+
+    
+    for (i in 1:k){
+      centros[i,1] <- apply(muestras[centrosAsignados == i,],2,"mean")[1]
+      centros[i,2] <- apply(muestras[centrosAsignados == i,],2,"mean")[2]
+    }
+    
+
+    
+    ## calculo el error como la suma de las distancias a los centros
+    
+    error[n] <- 0
+    for (i in 1:k){
+      error[n] <- error[n] + sum(distanciaACentros[centrosAsignados == i,i])
+    }
     
     
-    if ((k > 1) && (acum[k] == acum[k-1])){
+    if (n > 1 && error[n] == error[n-1]){
       break
     }
-    k <- k+1
+    
+    n <- n + 1
   }
-  return(list(m[,4],centros,plot(acum,type = "l")))
+  return(list(centrosAsignados, centros, error[n], plot(error,type = "l")))
 }
 
+kmedias(datosDF,3)[[4]]
+ggplot(datosDF,aes(X1,X2)) + geom_point(color = kmedias(datosDF,3)[[1]])
+  
+pengu <- penguins
+
+pengu <- pengu[,c("bill_length_mm", "flipper_length_mm")]
+
+pengu <- pengu[!is.na(pengu[,1]) & !is.na(pengu[,2]),]
+
+kmedias(pengu[,c("bill_length_mm", "flipper_length_mm")],3)
+
+col <- kmedias(pengu[,c("bill_length_mm", "flipper_length_mm")],3)[[1]]
+
+plot(pengu,col = col)
+
+ggplot(pengu,aes(bill_length_mm,flipper_length_mm,color = kmedias(pengu[,c("bill_length_mm", "flipper_length_mm")],3)[[1]])) + geom_point()
 
 
-# flipper vs bill length
-
-mat <- pengu[,c("flipper_length_mm","bill_length_mm")]
-set.seed(120)
-kmedias(mat,3)
-color <- kmedias(mat,3)[[1]]
-
-plot(mat[!is.na(mat[,1]) & !is.na(mat[,2]),],col = color)
-
+pengu <- pengu[,c("bill_length_mm", "flipper_length_mm")]
